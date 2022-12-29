7657b53714810ec4ca6840867b7980015ffb22f3
johnnyyork13 <johnnyyork13@hotmail.com>
2022-12-29 17:51:32 -0500

    add GUI for RPS game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+let result = document.getElementById('result');
+let playerScoreHead = document.getElementById('playerScore');
+let computerScoreHead = document.getElementById('computerScore'); 
+const resetBtn = document.getElementById('resetBtn');
+let displayPlayer = document.getElementById('displayPlayer');
+let displayComputer = document.getElementById('displayComputer');
+let conditionText = document.getElementById('condition');
+
 function getComputerChoice() 
 {
     let randomChoice = Math.floor(Math.random() * 3);
@@ -15,51 +23,86 @@ function getComputerChoice()
     return computer;
 }
 
-function playRound(playerSelection, computerSelection)
+function checkResult(player, computerSelection)
 {
-    player = playerSelection.toUpperCase();
-    console.log("Player played " + player);
-    console.log("Computer played " + computerSelection);
-
     if (player == computerSelection) {
-        console.log("Draw!");
-    } else if ((player == "ROCK" && computerSelection == "SCISSORS") || 
-    (player == "PAPER" && computerSelection == "ROCK") ||
-    (player == "SCISSORS" ** computerSelection == "PAPER")) {
-        console.log("Point to player!");
-        playerScore++;
-    } else if ((computerSelection == "ROCK" && player == "SCISSORS") ||
-    (computerSelection == "PAPER" && player == "ROCK") ||
-    (computerSelection == "SCISSORS" && player == "PAPER")) {
-        console.log("Point to computer!");
-        computerScore++;
-    } else {
-        console.log("Please enter a valid choice.")
-    }
-
-}
-
-function game()
-{
-    for (i = 0; i < 5; i++)
+        result.textContent = 'NEITHER COMPETITOR GETS A POINT';
+        conditionText.style.fontSize = "30px";
+        conditionText.textContent = "DRAW"
+        } else if ((player == "ROCK" && computerSelection == "SCISSORS") || 
+        (player == "PAPER" && computerSelection == "ROCK") ||
+        (player == "SCISSORS" && computerSelection == "PAPER")) {
+            result.textContent = 'YOU HAVE GAINED A POINT OVER YOUR ENEMY'
+            playerScore++;
+            conditionText.style.fontSize = "30px";
+            conditionText.textContent = "BEATS";
+        } else if ((computerSelection == "ROCK" && player == "SCISSORS") ||
+        (computerSelection == "PAPER" && player == "ROCK") ||
+        (computerSelection == "SCISSORS" && player == "PAPER")) {
+            result.textContent = 'YOUR ENEMY GAINS A POINT'
+            computerScore++;
+            conditionText.style.fontSize = "18px";
+            conditionText.textContent = "IS BEATEN BY";
+        }
+    
+    if (playerScore === 5)
     {
-        playerSelection = prompt("Choose Rock, Paper, or Scissors.");
-        computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log("Player Score is " + playerScore);
-        console.log("Computer Score is " + computerScore);
+        result.textContent = "YOU HAVE BEATEN YOUR ENEMY";
+    } else if (computerScore === 5) {
+        result.textContent = "YOUR ENEMY HAS BEATEN YOU";
     }
 
-    if (playerScore > computerScore){
-        console.log("Player Wins!");
-    } else if (computerScore > playerScore){
-        console.log("Computer Wins!");
-    } else {
-        console.log("The game is a draw!");
-    }
+    displayPlayer.style.visibility = "visible";
+    displayPlayer.textContent = player;
+
+    displayComputer.style.visibility = "visible";
+    displayComputer.textContent = computerSelection;
+
+    playerScoreHead.textContent = playerScore.toString();
+    computerScoreHead.textContent = computerScore.toString();
+    
 }
+
+function resetGame()
+{
+    resetBtn.style.visibility = "visible";
+    resetBtn.addEventListener('click', function(){
+        resetBtn.style.visibility = "hidden";
+        result.textContent = "";
+        conditionText.style.fontSize = "30px";
+        conditionText.textContent = "BEGIN";
+        displayPlayer.style.visibility = "hidden";
+        displayComputer.style.visibility = "hidden";
+        playerScore = 0;
+        computerScore = 0;
+        playerScoreHead.textContent = playerScore.toString();
+        computerScoreHead.textContent = computerScore.toString();
+        
+    })
+
+}
+
+const choiceList = document.querySelectorAll('.choiceDiv');
+
+choiceList.forEach((choice) => {
+    choice.addEventListener('click', function(){
+        if(playerScore < 5 && computerScore < 5) {
+            let player = choice.id.toUpperCase();
+            let computerSelection = getComputerChoice();
+            checkResult(player, computerSelection);
+
+            if (playerScore === 5)
+            {
+                resetGame();
+            } else if (computerScore === 5) {
+                resetGame();
+            }
+        }
+
+        })
+    })
 
 let playerScore = 0;
 let computerScore = 0;
 
-game();
+
